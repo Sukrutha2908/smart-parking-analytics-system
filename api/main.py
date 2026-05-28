66d733b27ee89449cec7b8863263f25ddf71de01
Sukrutha <sukrthat@gmail.com>
2026-05-28 20:08:53 +0530

    Updated FastAPI endpoints

--- api/main.py
+++ api/main.py
@@ -5,3 +5,18 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Smart Parking Analytics API Running"}
+
+@app.get("/parking-slots")
+def parking_slots():
+    return {
+        "total_slots": 100,
+        "occupied_slots": 65,
+        "available_slots": 35
+    }
+
+@app.get("/vehicles")
+def vehicles():
+    return {
+        "vehicle_count": 250,
+        "active_vehicles": 48
+    }
